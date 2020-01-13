79b0af074bb846eff744a66b6e41034aca0f5c84
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-13 23:44:26 +0000

    first commit

---
+++ lib/musical_artist_list.rb
@@ -0,0 +1,6 @@
+require "musical_artist_list/version"
+
+module MusicalArtistList
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/musical_artist_list/version.rb
@@ -0,0 +1,3 @@
+module MusicalArtistList
+  VERSION = "0.1.0"
+end
