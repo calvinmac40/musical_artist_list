e31861930dacae2cd4e72018e030f8164955a5ee
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-14 02:06:06 +0000

    menu fixes

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -8,14 +8,18 @@ class MusicalArtistList::CLI
   
   def menu
     input = nil
-    while input != "exit"
-    puts "Choose the next group of artist!"
-    input = gets.chomp
-    case input
-    when "1"
-      puts "A list artist"
+      while input != "exit"
+        puts "Choose the next group of artist!"
+        input = gets.chomp
+      case input
+      when "a"
+        puts "A list artist"
+      when "b"
+        puts "B-list artist"
+      when "c"
+        puts "C-list artist"
+      end
+    end
   end
-end
-end
   
 end
