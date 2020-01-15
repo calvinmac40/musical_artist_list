beea6201b3974c1fc8d399532a52a4aebc6c65c3
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-15 23:53:04 +0000

    Functionality works without scraping right now

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -11,7 +11,7 @@ class MusicalArtistList::CLI
       while input != "exit"
         input = gets.chomp
         selection = "a".."z"
-        selection.map do |letter|
+        selection.map do |letter| 
           if letter == input
             puts "#{letter.upcase}-list artist!"
             artist_selector
@@ -21,23 +21,16 @@ class MusicalArtistList::CLI
     end
     
     def artist_selector
-      puts "Thank you for your selection. Now choose an artist you would like to know more about by selecting the number next to that artist's name."
       selection = nil
+      puts "Thank you for your selection. Now choose an artist you would like to know more about by selecting the number next to that artist's name."
       while selection != "back"
-      single_artist = 1..array.length
-      new_array = []
-      single_artist.each_with_index do |artist,index|
-        if index == selection
-          "#{index}. #{artist}"
       selection = gets.chomp
-      puts "Thank you!"
       if selection == "back"
-        puts "Welcome Music Enthusiasts! Find information on your favorite artists here! To find a particular artist choose the first letter of the artist name."
+        puts "Welcome Music Enthusiasts!" 
+        puts "Find information on your favorite artists here! To find a particular artist choose the first letter of the artist name."
       else
         puts "To choose another artist select the number next to that artist name or select back to return to the main menu."
       end
     end
-    end
   end
-end
 end
