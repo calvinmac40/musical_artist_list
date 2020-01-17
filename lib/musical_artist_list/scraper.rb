fe7fa589eeb97f4ab3a7182f822fc2cc52269fa3
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-17 14:40:11 +0000

    scraping method changes

--- lib/musical_artist_list/scraper.rb
+++ lib/musical_artist_list/scraper.rb
@@ -2,6 +2,8 @@ class MusicalArtistList::Scraper
   
   def self.scrape 
     page = Nokogiri::HTML(open("https://www.udiscovermusic.com/artists-a-z/"))
-   binding.pry 
+    names = page.css(".ud-artist-page-columns").text.split
+    artist_list = names.map.with_index(1){|artist,index| "#{index}. #{artist}"}.drop(5)
+    binding.pry
   end
 end
