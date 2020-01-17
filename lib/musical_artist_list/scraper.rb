cdfdc5bdd801838a0cd4964113583063dad02b59
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-17 00:40:04 +0000

    hey

--- lib/musical_artist_list/scraper.rb
+++ lib/musical_artist_list/scraper.rb
@@ -2,6 +2,9 @@ class MusicalArtistList::Scraper
   
   def self.scrape 
     page = Nokogiri::HTML(open("https://www.udiscovermusic.com/artists-a-z/"))
-    
+    names = page.css(".ud-artist-page-columns")
+    binding.pry
   end
+
+
 end
