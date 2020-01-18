ff69147ca0bf6cd9db804c71c567cd5cf78ce342
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-18 19:18:12 +0000

    got the scraper to pull the artist info to work properly now I just have to figure out how to make it work with the CLI the way I intend it to work

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -2,7 +2,7 @@ class MusicalArtistList::CLI
   
   def call 
     puts "Welcome Music Enthusiasts!"
-    puts "Find information on your favorite artists here! To find a particular artist choose the first letter of the artist name."
+    puts 
     MusicalArtistList::Scraper.scrape
     menu
   end
--- lib/musical_artist_list/scraper.rb
+++ lib/musical_artist_list/scraper.rb
@@ -8,12 +8,21 @@ class MusicalArtistList::Scraper
     puts "#{index}. #{artist.text}" 
      end 
     }.compact
-    description = names.css("p")
-    binding.pry
+    
+    intro = page.css("p")[1].text
+    
+    aretha_franklin = page.css("p")[4].text #Aretha Franklin Bio.
+    whitney_houston = page.css("p")[10].text.gsub("\"", "") #Whitney Houston Bio.
+    otis_redding = page.css("p")[16].text
+    marvin_gaye = page.css("p")[20].text.gsub("\"", "")
+    staples_sisters = page.css("p")[26].text.gsub("\"", "")
+    the_emotions = page.css("p")[29].text.gsub("\"", "")
+    donna_summer = page.css("p")[33].text.gsub("\"", "")
+    stephanie_mills = page.css("p")[37].text
+    faith_evans = page.css("p")[42].text.gsub("\"", "")
+    john_legend = page.css("p")[47].text
+    laura_mvula = page.css("p")[52].text.gsub("\"", "")
+    curtis_mayfield = page.css("p").text.gsub("\"", "")
+    binding.pry 
   end
-  
-  # def self.find_by_letter(input)
-  #     @@all.select{|i| i == input}
-  #     binding.pry
-  #   end
 end
