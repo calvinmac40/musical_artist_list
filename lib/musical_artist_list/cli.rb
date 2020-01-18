f6cc72d0df50ac1eaf56308a8b699ef9367033be
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-18 14:54:56 +0000

    changed the website to scrape from and will impliment new funtionality based on the change

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -7,32 +7,12 @@ class MusicalArtistList::CLI
     menu
   end
   
+  
+  
   def menu
     input = nil
-      while input != "exit"
-        @input = gets.chomp
-        MusicalArtistList::Scraper.find_by_letter(@input)
-        # selection = "a".."z"
-        # selection.map do |letter| 
-        #   if letter == input
-        #     puts "#{letter.upcase}-list artist!"
-            artist_selector
-          end
-        end
-    #   end
-    # end
-    
-    def artist_selector
-      selection = nil
-      puts "Thank you for your selection. Now choose an artist you would like to know more about by selecting the number next to that artist's name."
-      while selection != "back"
-      selection = gets.chomp
-      if selection == "back"
-        puts "Welcome Music Enthusiasts!" 
-        puts "Find information on your favorite artists here! To find a particular artist choose the first letter of the artist name."
-      else
-        puts "To choose another artist select the number next to that artist name or select back to return to the main menu."
-      end
+    while input != "exit"
+      input = gets.chomp
     end
   end
 end
--- lib/musical_artist_list/scraper.rb
+++ lib/musical_artist_list/scraper.rb
@@ -3,15 +3,17 @@ class MusicalArtistList::Scraper
   @@all = []
   
   def self.scrape 
-    page = Nokogiri::HTML(open("https://www.udiscovermusic.com/artists-a-z/"))
-    names = page.css(".ud-artist-page-columns").text.split
-    artist_list = names.map.with_index(1){|artist,index| "#{index}. #{artist}"}
-    @@all << artist_list
-    #binding.pry
-  end
-  
-  def self.find_by_letter(input)
-    @@all.select{|i| i == input}
+    page = Nokogiri::HTML(open("https://www.biography.com/news/aretha-franklin-black-singers-church"))
+    names = page.css("h2").map.with_index(1){|artist,index| unless index > 12   
+    puts "#{index}. #{artist.text}" 
+     end 
+    }.compact
+    description = names.css("p")
     binding.pry
   end
+  
+  # def self.find_by_letter(input)
+  #     @@all.select{|i| i == input}
+  #     binding.pry
+  #   end
 end
