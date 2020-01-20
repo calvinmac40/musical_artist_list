bb411db8a60415b67b6fce0e4815f8b0f60399a4
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-20 14:05:33 +0000

    more changes

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -12,25 +12,25 @@ class MusicalArtistList::CLI
     menu
   end
   
-  def blues_artist
-    puts "Make your selection"
-    puts <<-DOC.gsub /^\s*/, ''
-    1. Fats Domino is a blues artist that my son did a history report on.
-    2. My favorite blues artist is....HMMm I don't have one.
-    DOC
-  end
+  # def blues_artist
+  #   puts <<-DOC.gsub /^\s*/, ''
+  #   1. Fats Domino is a blues artist that my son did a history report on.
+  #   2. My favorite blues artist is....HMMm I don't have one.
+  #   DOC
+  #   #@artist_bios = MusicalArtistList::Scraper.scrape
+  # end
   
   def menu
     input = nil
     while input != "exit"
-        puts "Please make your selection:"
-          input = gets.chomp
+         puts "Please make your selection:"
+         input = gets.chomp
       if input == "exit"
          goodbye
       elsif input == "1"
-        blues_artist
+         blues_artist
       elsif input == "2"
-        puts "Not sure what you're looking for? Please try again."
+         puts "Not sure what you're looking for?"
       end
     end
   end
