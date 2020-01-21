c7c09f3571313c53be9b2239cc65fa3f4cd4dc5a
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-21 01:42:52 +0000

    new addition

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -1,37 +1,42 @@
 class MusicalArtistList::CLI 
   
-  attr_accessor :name
+  attr_accessor :artist, :name 
   
   def call 
     puts "Welcome Music Enthusiasts!"
-    MusicalArtistList::Scraper.scrape
+    # MusicalArtistList::Scraper.display
     menu
   end
   
-  # def artist_list
-  # @artist = MusicalArtistList::Scraper.scrape
-  # @artist.each_with_index(1) do |artist,index|
-  #   puts "#{index}. #{artist.name} - #{artist.bio}"
-  # end
-  # end
+  def artist_list
+    @artist_info = MusicalArtistList::Scraper.display
+    @artist_info.map.with_index do |names,index|
+    puts  "#{index}. #{names.name}"
+  end
+    # puts <<-DOC 
+    #   1. This is the first choice.
+    # DOC
+    
+    # puts <<-DOC
+    #   2. THis is the second choice.
+    # DOC
+    #MusicalArtistList::Scraper.display
+    # @artist = MusicalArtistList::Scraper.display
+    # @artist.map.with_index do |n,i|
+    #   "#{i}. #{n.name}"
+    #   end
+  end
   
-  # def blues_artist
-  #   puts <<-DOC.gsub /^\s*/, ''
-  #   1. Fats Domino is a blues artist that my son did a history report on.
-  #   2. My favorite blues artist is....HMMm I don't have one.
-  #   DOC
-  #   #@artist_bios = MusicalArtistList::Scraper.scrape
-  # end
   
   def menu
    input = nil 
     while input != "exit"
-         puts "Please enter a number for the artist you'd like to know more about!"
+         puts "Please enter "
          input = gets.strip
        if input == "exit"
          goodbye
-       elsif input.to_i > 0 
-         puts @artist[input.to_i - 1]
+      elsif input.to_i > 0 
+      puts @artist_info[input.to_i - 1]
         elsif input == "list"
          artist_list
        else 
--- lib/musical_artist_list/scraper.rb
+++ lib/musical_artist_list/scraper.rb
@@ -1,15 +1,25 @@
 class MusicalArtistList::Scraper
   
+  attr_accessor :name, :bio
+  
   def self.display
-    self.scrape
+    artist_info = []
+    artist_info << self.scrape
+    artist_info
+    binding.pry
   end
   
+
+  
   def self.scrape
-    artist_info = []
     page = Nokogiri::HTML(open("https://www.pbs.org/theblues/songsartists/songsbioalpha.html"))
-    names = page.css("span.textName")
-    binding.pry
-    
-    artist_info
+    artists = self.new 
+    artists.name = page.css("span.textName")
+    artists.bio = page.css("p.textSm").text.gsub("\n\t\t","")
+    artists
+    #binding.pry
   end
 end
+
+# ./bin/musical_artist_list
+# cd musical_artist_list
