e5cfa801e97c1d9c13b3e627761746f5670dd0c1
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-25 10:39:33 +0000

    Made some changes to the spacing between the user input selection and the display of the artist list.

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -1,7 +1,10 @@
 class MusicalArtistList::CLI 
   
-  def call 
-    puts "Welcome Music Enthusiasts!"
+  def call
+    puts "-----------------------------"
+    puts "* Welcome Music Enthusiasts! *"
+    puts "-----------------------------"
+    puts ""
     menu
   end
   
@@ -10,12 +13,14 @@ class MusicalArtistList::CLI
    input = nil 
     while input != "exit"
          puts "To see a list of artist enter list."
+         puts ""
          input = gets.strip
        if input == "exit"
          goodbye
       elsif input.to_i > 0 
        @artist_info[input.to_i - 1]
         elsif input == "list"
+        puts ""
          artist_list
          else 
          puts "Not sure what you want?"
@@ -35,10 +40,12 @@ class MusicalArtistList::CLI
   def artist_bio
     input = nil 
     while input != "exit"
-    
+    puts ""
     puts "To make a selection enter a number between 1 - 79 or type list to see the menu again or type exit."
+    puts ""
     input = gets.strip
-    if input.to_i > 0 && input.to_i < 80 
+    if input.to_i > 0 && input.to_i < 80
+      puts ""
       puts @artist_info.bio[input.to_i - 1]
       elsif input == "list"
       artist_list
