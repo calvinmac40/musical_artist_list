96100c64b10cb004a92046a75ba980cf52fac9b7
Calvin McDowell <calvinmac40@hotmail.com>
2020-02-02 18:52:53 +0000

    controller is working fine

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -2,28 +2,33 @@ class MusicalArtistList::CLI
   
   def call
     MusicalArtistList::Scraper.scrape #scrapes all the information at once.
-    puts "" # puts an empty space between lines.
+    puts ""     # puts an empty space between lines.
     puts "-----------------------------"
     puts "* Welcome Music Enthusiasts! *"
     puts "-----------------------------"
     puts ""
-    menu # calls on the mehod menu
+    menu        # calls on the mehod menu
   end
   
   def menu
-    input = nil #gives the variable input a value of nil
-    puts "To see a list of artist enter list" # promps the user to enter a command to display a list of artists.
+    input = nil     #gives the variable input a value of nil
+    puts "To see a list of artist enter list"     # promps the user to enter a command to display a list of artists.
     puts ""
-    while input != "exit" # loops until a user inputs certain commands.
-    input = gets.strip.downcase # waits for user input.
-    if input == "exit" # if statement conditional evaluates user input.
-      goodbye # calls on the method if the command 'exit' is entered by the user.
-      elsif  #input.to_i > 0 && input.to_i < 80
+    while input != "exit"         # loops until a user inputs certain commands.
+    input = gets.strip.downcase         # waits for user input.
+    if input == "exit"      # if statement conditional evaluates user input.
+      goodbye    # calls on the method if the command 'exit' is entered by the user.
+      elsif  input.to_i > 0 && input.to_i < 80
+      puts ""
+      puts "Not sure what you're looking for?"
       puts "Please enter the command 'list'!"
       puts ""
       elsif input == "list"
       puts ""
       artist_names
+      puts ""
+      puts "To learn more enter a number associated with an artist."
+      puts ""
       else 
       puts "Not sure what you're looking for?"
       puts "Please enter the command 'list'!"
--- lib/musical_artist_list/scraper.rb
+++ lib/musical_artist_list/scraper.rb
@@ -4,7 +4,7 @@ attr_accessor :name, :bio
 
 def self.scrape
     page = Nokogiri::HTML(open("https://www.pbs.org/theblues/songsartists/songsbioalpha.html"))
-    page.search('p.textSm')[0..(-4)].each do |card|
+    page.search('p.textSm')[0..(-5)].each do |card|
       artist_name = card.css('span.textName').text.strip
       artist_bio = card.text.gsub("\t","")
       MusicalArtistList::Artist.new(artist_name,artist_bio)
