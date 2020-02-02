a7ea0927be44a7ae13d342ab2b414ad061387c32
Calvin McDowell <calvinmac40@hotmail.com>
2020-02-02 16:32:43 +0000

    creating comments to map out what each line is doing

--- lib/musical_artist_list/cli.rb
+++ lib/musical_artist_list/cli.rb
@@ -1,65 +1,43 @@
 class MusicalArtistList::CLI 
   
   def call
-    MusicalArtistList::Scraper.scrape
-    puts ""
+    MusicalArtistList::Scraper.scrape #scrapes all the information at once.
+    puts "" # puts an empty space between lines.
     puts "-----------------------------"
     puts "* Welcome Music Enthusiasts! *"
     puts "-----------------------------"
     puts ""
-    menu
+    menu # calls on the mehod menu
   end
   
-
   def menu
-   input = nil 
-    while input != "exit"
-         puts "To see a list of artist enter list."
-         puts ""
-         input = gets.strip.downcase
-       if input == "exit"
-         goodbye
-        elsif input == "list"
-        puts ""
-        artist_names
-         else 
-         puts "Not sure what you want?"
-       end
+    input = nil #gives the variable input a value of nil
+    puts "To see a list of artist enter list" # promps the user to enter a command to display a list of artists.
+    puts ""
+    while input != "exit" # loops until a user inputs certain commands.
+    input = gets.strip.downcase # waits for user input.
+    if input == "exit" # if statement conditional evaluates user input.
+      goodbye # calls on the method if the command 'exit' is entered by the user.
+      elsif  #input.to_i > 0 && input.to_i < 80
+      puts "Please enter the command 'list'!"
+      puts ""
+      elsif input == "list"
+      puts ""
+      artist_names
+      else 
+      puts "Not sure what you're looking for?"
+      puts "Please enter the command 'list'!"
+      puts ""
+     end
     end
   end
-  
   
   def artist_names
-      @artists = MusicalArtistList::Artist
-      @artists.all.map.with_index(1) do |artist,index|
-      puts  "#{index}. #{artist.name}".strip
-    end
-    artist_bios
-  end
-  
-  
-  
-  def artist_bios
-    input = nil 
-    while input != "exit"
-    puts ""
-    puts "To make a selection enter a number between 1 - 79 or type lis to see the menu again or type exit."
-    puts ""
-    input = gets.strip
-    if input == "list"
-     artist_names 
-    elsif input.to_i > 0 && input.to_i < 80
-     @artists_bio = MusicalArtistList::Artist
-     @artists_bio.all.map.with_index(1) do |artist,index|
-       puts "#{index}. #{artist.bio}"
-     end
-      elsif input == "exit"
-      goodbye
-      exit
+    @artists = MusicalArtistList::Artist 
+    @artists.all.map.with_index(1) do |artist,index|
+    puts "#{index}. #{artist.name}"
     end
   end
-  end
-  
   
   def goodbye
     puts "Goodbye!"
